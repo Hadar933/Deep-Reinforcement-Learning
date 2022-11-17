bd9d8953422521ebe5308b4faa07f91c7259ca03
sagivy <sagiv.yaari@gmail.com>
2022-11-17 11:39:16 +0200

    added sampling in main

--- ex1/DQN.py
+++ ex1/DQN.py
@@ -1,14 +1,12 @@
 import random
-import torch
 import gym
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 from collections import deque
 from typing import Tuple, List
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+from tqdm import tqdm
+import numpy as np
 
 class ExperienceReplay:
     def __init__(self, size: int):
@@ -47,8 +45,47 @@ class DQN:
         Q_net.compile(loss='mse', optimizer=Adam(learning_rate=self.lr))
         return Q_net
 
+    def learn(self, batch_size):
+        pass
+
+    def get_action(self, state, epsilon = 0):
+        if epsilon > random.random():
+            action = self.env.action_space.sample()
+        else:
+            action = np.argmax(self.Qs_net.predict(state))
+        return action
+
+
+
 
 if __name__ == '__main__':
     dqn = DQN(1, 1, 5, 0.01, [1, 2, 3])
+    n_episodes = 500
+    for ep in tqdm(range(n_episodes)):
+        obs = dqn.env.reset()
+        for step in range(n_steps):
+            act = dqn.get_action(s)
+            next_state, reward, done, info = dqn.env.step(act)
+            dqn.D.append(state,act,reward,next_state,done)
+            dqn.learn(batch_size=1)
+            target = reward if done else reward + gamma * np.max(Q[next_state])
+            Q[state, action] = (1 - alpha) * Q[state, action] + alpha * target
+            state = next_state
+            if done:  # either reached goal or fell to a hole
+                steps_to_goal = step if state == GOAL_STATE else n_steps
+                steps_to_goal_arr_over_100_episodes.append(steps_to_goal)
+                break
+            if step == n_steps - 1 and not done:  # ran out of steps without reaching goal
+                steps_to_goal = n_steps
+                steps_to_goal_arr_over_100_episodes.append(steps_to_goal)
+        if ep % 100 == 0:
+            mean_steps = np.mean(steps_to_goal_arr_over_100_episodes)
+            mean_steps_to_goal_arr.append(mean_steps.item())
+            steps_to_goal_arr_over_100_episodes = []
+
+        epsilon = linear_decay_eps
+        ep_rew_arr.append(ep_rew)
+    env.close()
+    return Q, ep_rew_arr, mean_steps_to_goal_arr
     s = dqn.env.observation_space.sample()
     a = dqn.Qs_net.predict(s)
