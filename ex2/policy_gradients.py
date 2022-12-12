0732673078d3e6d47c6b9398912218045552046b
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2022-12-12 18:53:17 +0200

    added ex2 skeleton

--- ex2/policy_gradients.py
+++ ex2/policy_gradients.py
@@ -1,7 +1,23 @@
 import gym
 import numpy as np
+# import tensorflow as tf
 import tensorflow.compat.v1 as tf
+# from tensorf
 import collections
+from typing import List
+import json
+from os import path
+from sorcery import dict_of
+
+try:
+    import wandb
+
+    is_wandb = True
+except ImportError:
+    print('Please install wandb (pip install wandb && wandb login) to log to weights and biases')
+    is_wandb = False
+
+import datetime
 
 # optimized for Tf2
 tf.disable_v2_behavior()
@@ -18,7 +34,6 @@ class PolicyNetwork:
         self.learning_rate = learning_rate
 
         with tf.variable_scope(name):
-
             self.state = tf.placeholder(tf.float32, [None, self.state_size], name="state")
             self.action = tf.placeholder(tf.int32, [self.action_size], name="action")
             self.R_t = tf.placeholder(tf.float32, name="total_rewards")
@@ -41,24 +56,83 @@ class PolicyNetwork:
             self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
 
+class ValueNetwork:
+    def __init__(self, state_size, hidden_layers=[12, 12], learning_rate=0.0004, name='value_network'):
+        self.state_size = state_size
+        self.learning_rate = learning_rate
+        self.layers = []
+
+        with tf.variable_scope(name):
+            self.state = tf.placeholder(tf.float32, [None, self.state_size], name="state")
+            self.value = tf.placeholder(tf.int32, 1, name="value")
+            self.R_t = tf.placeholder(tf.float32, name="rewards-to-go")
+
+            tf2_initializer = tf.keras.initializers.glorot_normal(seed=0)
+            input_size = self.state_size
+            layer_input = self.state
+            for i in range(len(hidden_layers) - 1):
+                self.layers.append({
+                    'w': tf.get_variable(f"w{i}", [input_size, hidden_layers[i]], initializer=tf2_initializer),
+                    'b': tf.get_variable(f"b{i}", [hidden_layers[i]], initializer=tf2_initializer), })
+                self.layers[i]['z'] = tf.add(tf.matmul(layer_input, self.layers[i]['w']), self.layers[i]['b'])
+                layer_input = tf.nn.relu(self.layers[i]['z'])
+                input_size = hidden_layers[i]
+            i += 1
+            self.value = tf.layers.dense(layer_input, units=1, activation=None)
+            self.loss = tf.reduce_mean((self.R_t - self.value) ** 2)
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
+
+
 def run():
     # Define hyperparameters
+
     state_size = 4
     action_size = env.action_space.n
 
     max_episodes = 5000
     max_steps = 501
     discount_factor = 0.99
-    learning_rate = 0.0004
-
+    p_learning_rate = 0.0002
+    v_learning_rate = 0.001
+    n_v_iter = 1
+    v_update_interval = 1
     render = False
+    baseline = True
+    actor_critic = True
+    v_layers = [12, 12, 12]
+
+    param_dict = dict_of(max_episodes, max_steps, discount_factor, p_learning_rate, v_learning_rate, n_v_iter,
+                         v_update_interval, render, baseline, actor_critic, v_layers)
 
     # Initialize the policy network
     tf.reset_default_graph()
-    policy = PolicyNetwork(state_size, action_size, learning_rate)
+    policy = PolicyNetwork(state_size, action_size, p_learning_rate)
+    if baseline:
+        value_net = ValueNetwork(state_size, hidden_layers=v_layers, learning_rate=v_learning_rate)
+
+    ### Logging
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    train_log_dir = './logs/' + current_time + '/train'
+    summary_writer = tf.summary.FileWriter(train_log_dir)
+    # Save params
+    if is_wandb:
+        wandb.init(project="BGURL-Ex2", config=param_dict)
+    with open(path.join(train_log_dir, 'params.json'), 'w') as f:
+        f.write(json.dumps(param_dict, indent=4))
+    episode_reward = tf.Variable(0, dtype=tf.float32)
+    avg_reward = tf.Variable(0, dtype=tf.float32)
+    p_loss_th = tf.Variable(0, dtype=tf.float32)
+    v_loss_th = tf.Variable(0, dtype=tf.float32)
+    _ = tf.summary.scalar('Episode_reward', episode_reward)
+    _ = tf.summary.scalar('Avg_Reward', avg_reward)
+    _ = tf.summary.scalar('Policy_loss', p_loss_th)
+    _ = tf.summary.scalar('ValueFn_loss', v_loss_th)
+
+    summaries = tf.summary.merge_all()
 
     # Start training the agent with REINFORCE algorithm
     with tf.Session() as sess:
+        total_steps = 0
         sess.run(tf.global_variables_initializer())
         solved = False
         Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
@@ -81,14 +155,19 @@ def run():
 
                 action_one_hot = np.zeros(action_size)
                 action_one_hot[action] = 1
-                episode_transitions.append(Transition(state=state, action=action_one_hot, reward=reward, next_state=next_state, done=done))
+                episode_transitions.append(
+                    Transition(state=state, action=action_one_hot, reward=reward, next_state=next_state, done=done))
                 episode_rewards[episode] += reward
 
                 if done:
                     if episode > 98:
                         # Check if solved
-                        average_rewards = np.mean(episode_rewards[(episode - 99):episode+1])
-                    print("Episode {} Reward: {} Average over 100 episodes: {}".format(episode, episode_rewards[episode], round(average_rewards, 2)))
+                        average_rewards = np.mean(episode_rewards[(episode - 99):episode + 1])
+                    total_steps += step
+                    print(
+                        "Episode {} Reward: {} Average over 100 episodes: {}".format(episode, episode_rewards[episode],
+                                                                                     round(average_rewards, 2)))
+
                     if average_rewards > 475:
                         print(' Solved at episode: ' + str(episode))
                         solved = True
@@ -96,13 +175,63 @@ def run():
                 state = next_state
 
             if solved:
+                wandb.log({
+                    'Episode Reward': episode_reward,
+                    'Average Reward': avg_reward,
+                    'total Steps': total_steps
+                })
+                ths = [episode_reward, avg_reward]
+                vals = [episode_rewards[episode], average_rewards]
+                for th, val in zip(ths, vals):
+                    sess.run(th.assign(val))
+                summary_writer.add_summary(sess.run(summaries), global_step=episode)
                 break
 
             # Compute Rt for each time-step t and update the network's weights
+
             for t, transition in enumerate(episode_transitions):
-                total_discounted_return = sum(discount_factor ** i * t.reward for i, t in enumerate(episode_transitions[t:])) # Rt
-                feed_dict = {policy.state: transition.state, policy.R_t: total_discounted_return, policy.action: transition.action}
-                _, loss = sess.run([policy.optimizer, policy.loss], feed_dict)
+                # At = Rt-Vt
+                # see: https://spinningup.openai.com/en/latest/spinningup/rl_intro3.html#baselines-in-policy-gradients
+                if actor_critic:
+                    R_t = transition.reward + (1 - transition.done) * discount_factor * sess.run(value_net.value, {
+                        value_net.state: transition.next_state})  # R + v'(S')
+                else:
+                    R_t = sum(discount_factor ** i * t.reward for i, t in enumerate(episode_transitions[t:]))  # Rt
+                if baseline or actor_critic:
+                    feed_dict = {value_net.state: transition.state}
+                    A_t = R_t - sess.run(value_net.value, feed_dict)
+
+                feed_dict = {policy.state: transition.state, policy.R_t: A_t if baseline else R_t,
+                             policy.action: transition.action}
+                _, p_loss = sess.run([policy.optimizer, policy.loss], feed_dict)
+                if (baseline or actor_critic) and episode % v_update_interval == 0:
+                    for _ in range(n_v_iter):
+                        feed_dict = {value_net.state: transition.state, value_net.R_t: R_t}
+                        _, v_loss = sess.run([value_net.optimizer, value_net.loss], feed_dict)
+            if baseline:
+                ths = [episode_reward, avg_reward, p_loss_th, v_loss_th]
+                vals = [episode_rewards[episode], average_rewards, p_loss, v_loss]
+                if is_wandb:
+                    wandb.log({
+                        'Episode Reward': episode_reward,
+                        'Average Reward': avg_reward,
+                        'Policy Loss': p_loss,
+                        'Value Fn Loss': v_loss,
+                        'total Steps': total_steps
+                    })
+            else:
+                ths = [episode_reward, avg_reward, p_loss_th]
+                vals = [episode_rewards[episode], average_rewards, p_loss]
+                if is_wandb:
+                    wandb.log({
+                        'Episode Reward': episode_reward,
+                        'Average Reward': avg_reward,
+                        'Policy Loss': p_loss,
+                        'total Steps': total_steps
+                    })
+            for th, val in zip(ths, vals):
+                sess.run(th.assign(val))
+            summary_writer.add_summary(sess.run(summaries), global_step=episode)
 
 
 if __name__ == '__main__':
